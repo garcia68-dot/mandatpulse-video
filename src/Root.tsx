7e0788f49c68b9e2efd0c90696798d91ab621545
garcia68-dot <garcia68@hotmail.fr>
2026-09-14 03:13:32 +0300

    Register LighAuraStory composition

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import {Composition} from 'remotion';
 import {MandatPulse} from './MandatPulse';
+import {LighAuraStory} from './LighAuraStory';
 
 export const Root: React.FC = () => (
-  <Composition
-    id="MandatPulse"
-    component={MandatPulse}
-    durationInFrames={720}
-    fps={30}
-    width={1080}
-    height={1350}
-  />
+  <>
+    <Composition
+      id="MandatPulse"
+      component={MandatPulse}
+      durationInFrames={720}
+      fps={30}
+      width={1080}
+      height={1350}
+    />
+    <Composition
+      id="LighAuraStory"
+      component={LighAuraStory}
+      durationInFrames={720}
+      fps={30}
+      width={1920}
+      height={1080}
+    />
+  </>
 );
